f6955ef19b4461bce2b7cbc1bbfd63551c6d1642
Mouddassar Baig <Baigmou@sheridancollege.ca>
2025-05-22 12:50:52 -0400

    added magic hand and results code

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -10,6 +10,7 @@ package card;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Mouddassar Baig
  */
+import java.util.Scanner;
 public class CardTrick {
     
     public static void main(String[] args)
@@ -24,13 +25,38 @@ public class CardTrick {
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             c.setSuit(Card.SUITS[(int)(Math.random() * 4)]);
             magicHand[i] = c;
-            
+           
         }
         
+        Scanner input = new Scanner(System.in);
+        
         //insert code to ask the user for Card value and suit, create their card
+        System.out.println("Enter a card value (1-13: ");
+        int uValue = input.nextInt();
+        System.out.println("Enter a suit 0-3 where 0 = hearts, 1=diamonds, 2=clubs, 3=spades): ");
+        int suitIndex = input.nextInt();
+        String uSuit = Card.SUITS[suitIndex];
+        Card uCard = new Card();
+        uCard.setValue(uValue);
+        uCard.setSuit(uSuit);
+        
         // and search magicHand here
+        boolean found = false;
+        for (int i=0; i<magicHand.length; i++) {
+            if (magicHand[i].getValue() == uCard.getValue() && magicHand[i].getSuit().equalsIgnoreCase(uSuit))
+            {
+                found = true;
+                break;
+            }
+        }
         //Then report the result here
+        if (found) {
+            System.out.println("Congrats! Your card is in the magic hand!");
+        } else {
+            System.out.println("Sorry, your card is NOT in the magic hand");
+        }
         // add one luckcard hard code 2,clubs
+        
     }
     
 }
