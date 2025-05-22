3810fd75fee6d2320a43a3c28e990752fca5d845
Mouddassar Baig <Baigmou@sheridancollege.ca>
2025-05-22 12:25:48 -0400

    Magic hand done

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -8,7 +8,7 @@ package card;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
+ * @author Mouddassar Baig
  */
 public class CardTrick {
     
@@ -20,7 +20,11 @@ public class CardTrick {
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue((int)(Math.random() * 13) + 1);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[(int)(Math.random() * 4)]);
+            magicHand[i] = c;
+            
         }
         
         //insert code to ask the user for Card value and suit, create their card
